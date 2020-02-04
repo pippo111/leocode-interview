7bcbe22924dbeadc51c499fc460cfcb3c7efc950
Filip Barańczuk <f.baranczuk@gmail.com>
2020-02-04 19:57:24 +0100

    Add some page styling

--- src/components/UI/Page.js
+++ src/components/UI/Page.js
@@ -3,7 +3,15 @@ import PropTypes from 'prop-types'
 import { Box, Text } from '@smooth-ui/core-sc'
 
 const PageTemplate = ({ title, children }) => (
-  <Box mx='auto' px={20} maxWidth={800}>
+  <Box
+    my={3}
+    mx='auto'
+    p={20}
+    maxWidth={800}
+    border='1px solid lightgray'
+    borderRadius={8}
+    boxShadow='0px 0px 14px 0px rgba(170,170,170,1)'
+  >
     {title && <Text variant='h1' textAlign='center'>{title}</Text>}
     {children}
   </Box>
