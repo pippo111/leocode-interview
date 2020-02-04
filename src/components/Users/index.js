784bdfe0aafe3179ee43e7e6c5b08a672b2552d8
Filip Barańczuk <f.baranczuk@gmail.com>
2020-02-04 19:17:15 +0100

    Implement user search

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import usersJson from '../../constants/users.json'
 
 import PageTemplate from '../PageTemplate'
 import SearchBox from '../SearchBox'
 
-const Users = () => (
-  <PageTemplate title='Users list'>
-    <SearchBox onChange={value => console.log(value)} />
-  </PageTemplate>
-)
+const Users = () => {
+  const [users, setUsers] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    setUsers(usersJson)
+  }, [])
+
+  return (
+    <PageTemplate title='Users list'>
+      <SearchBox onChange={value => setSearchTerm(value)} />
+      <ol>
+        {users
+          .filter(({ name }) =>
+            name.toLowerCase().includes(searchTerm.toLowerCase()))
+          .map(({ id, name, username }) => (
+            <li key={id}>{name} @{username}</li>
+          ))}
+      </ol>
+    </PageTemplate>
+  )
+}
 
 export default Users
