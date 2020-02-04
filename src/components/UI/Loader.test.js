aa2a8adc3b027ba6ce3d756629edcafd9b7b48c8
Filip Barańczuk <f.baranczuk@gmail.com>
2020-02-04 21:18:21 +0100

    Setup basic tests

---
+++ src/components/UI/Loader.test.js
@@ -0,0 +1,23 @@
+/* global describe, test, it, expect */
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
+
+import { Loader } from '.'
+
+describe('Loader', () => {
+  test('snapshot renders', () => {
+    const component = renderer.create(<Loader />)
+    const tree = component.toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders custom caption', () => {
+    const caption = 'Loading test...'
+
+    const component = mount(<Loader caption={caption} />)
+    expect(component.text()).toEqual(caption)
+  })
+})
